c7e469e719821f2a8a677cead72c7b2b660c5194
Alistair Evans <alistair.evans@enclave.io>
2024-01-11 10:59:44 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.InteropServices;
+
+var libSodiumVersion = GetSodiumVersion();
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine($"Loaded libsodium: {libSodiumVersion}");
+
+[DllImport("libsodium", CallingConvention = CallingConvention.Cdecl)]
+[DefaultDllImportSearchPaths(DllImportSearchPath.SafeDirectories)]
+static extern IntPtr sodium_version_string();
+
+static string GetSodiumVersion()
+{
+    var marshalledString = Marshal.PtrToStringAnsi(sodium_version_string());
+
+    if (marshalledString is null)
+    {
+        throw new InvalidOperationException("Cannot retrieve libsodium version");
+    }
+
+    return marshalledString;
+}
